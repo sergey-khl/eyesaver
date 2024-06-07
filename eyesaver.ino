e63ad4010cf35cd0fd472a3babbe162d539de507
Sergey <sergeykhlynovskiy@gmail.com>
2024-06-07 15:00:48 -0600

    done

---
+++ eyesaver.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int led = 13;
+  digitalWrite(led, HIGH);
+//  delay(10000);
+}
